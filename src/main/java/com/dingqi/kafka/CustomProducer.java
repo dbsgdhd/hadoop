488ae781abb3852bf6f0e5190c0fe26894a84fd6
babybattle <2816512220@qq.com>
2020-12-22 20:26:19 +0800

    hb

---
+++ src/main/java/com/dingqi/kafka/CustomProducer.java
@@ -0,0 +1,28 @@
+package com.dingqi.kafka;
+
+import org.apache.kafka.clients.producer.*;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class CustomProducer {
+
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "hadoop131:9092");//kafka集群，broker-list
+        props.put("acks", "all");
+        props.put("retries", 1);//重试次数
+        props.put("batch.size", 16384);//批次大小
+        props.put("linger.ms", 1);//等待时间
+        props.put("buffer.memory", 33554432);//RecordAccumulator缓冲区大小
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer = new KafkaProducer<>(props);
+        for (int i = 0; i < 100; i++) {
+            producer.send(new ProducerRecord<String, String>("first", Integer.toString(i), Integer.toString(i)));
+        }
+        producer.close();
+    }
+}
